f2c1384375c035dbdfb86aa8fea4dd5aa4a01f1c
MihirRao01 <mihirhrao@gmail.com>
2022-12-04 19:12:27 -0500

    wrote the set intersection algorithm

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main(){
     // windows file path
     //string filename = "C:\\Users\\mihir\\Downloads\\books1.csv";
     // mac file path
-    string filename = "../books1.csv";
+    string filename = "./books1.csv";
     cout<<"opening file"<<endl;
     inputFile.open(filename);
 
@@ -73,15 +73,42 @@ int main(){
     string source_book = "The Hunger Games";
     set<string> source_set = bg[source_book];
     set<string> visted;
+    visted.insert(source_book);
+    int threshold = 3;
 
+// set intersection logic : 
     // for each genre in source
         // for each book in that genre
-            // check if that book has been visted
-                // perform a set union with the source set
+            // check if that book has not been visted
+                // perform a set intersection with the source set
                     // if the result is >= 3 then add the book to the result vector
                 // add that book to visited
 
-    
+    for( auto g : source_set){
+       set<string> s1 = gb[g];
+       for(auto b : s1){
+        if(visted.count(b) == 0){
+            // perform a set intersection between the two books' genres
+            int intersectCount = 0;
+            set<string>s2 = bg[b];
+            for(auto g2 : s2){
+                if(source_set.count(g2)>0){
+                    intersectCount++;
+                }
+            }
+            if(intersectCount>=threshold){
+                related_book.push_back(b);
+            }
+            visted.insert(b);
+        }
+       }
+    }
+
+    // printing out the related books
+    cout<<"The books that are related to "<<source_book<<" are: "<<endl;
+    for(int i = 0; i<related_book.size(); i++){
+        cout<<related_book[i]<<endl;
+    }
 
     return 0;
 }
